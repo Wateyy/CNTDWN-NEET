7bdf24c51a46a3fc9d5685da4695e038c81f653a
Wateyy <discodiscord67321@gmail.com>
2024-12-25 00:21:02 +0530

    only neet push

--- config/exams.ts
+++ config/exams.ts
@@ -1,11 +1,7 @@
 export const exams = {
     jeeadv: {
       name: 'NEET UG',
-      date: new Date('2026-05-18T08:00:00')
-    },
-    jeemain: {
-      name: 'MAINS',
-      date: new Date('2026-01-22T09:00:00')
+      date: new Date('2026-05-01T14:00:00')
     }
   }
   
