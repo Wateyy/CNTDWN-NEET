e802725cd4eb72468b449d6f65b5bb54186bd0dd
Wateyy <discodiscord67321@gmail.com>
2024-12-28 13:30:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Final Countdown',
-  description: 'Countdown to important exams',
+  title: 'NEET 2026 Final Countdown',
+  description: 'Countdown to NEET 2026',
 }
 
 export default function RootLayout({
